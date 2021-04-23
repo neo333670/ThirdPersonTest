911c6b11d450be41a24e864aa63754a574c66e86
neo333670 <neo333670@gmail.com>
2021-04-23 21:49:16 +0800

    open the file

---
+++ ForTestTP/Intermediate/Build/Win64/ForTestTPEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.28.29914
+#include "D:/Create with code/UE4/ForTestTP/ThirdPersonTest/ForTestTP/Intermediate/Build/Win64/ForTestTPEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ ForTestTP/Source/ForTestTP/ForTestTPGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ForTestTPGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AForTestTPGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AForTestTPGameMode();
+};
+
+
+
